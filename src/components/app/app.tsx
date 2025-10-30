feafd6599acaa183b8159a4a0edeef56e85f9b89
CepreNGT <malitevs@gmail.com>
2025-10-30 22:00:00 +0500

    Create app.tsx

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppProps = {
+  placesCount: number;
+}
+
+function App({placesCount}: AppProps): JSX.Element {
+  return (
+    <MainScreen placesCount={placesCount}/>
+  );
+}
+
+export default App;
