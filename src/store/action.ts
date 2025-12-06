4ecb7f39f604c43dbec9cbdc227bbc6d1c7de063
CepreNGT <malitevs@gmail.com>
2025-12-06 17:05:52 +0500

    Added authorisationStatus to global state

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,8 +1,11 @@
 import { createAction } from '@reduxjs/toolkit';
 import { OffersList } from '../types/offer';
+import { AuthorizationStatus } from '../const';
 
 export const setCurrentCity = createAction<string>('cities/setCurrentCity');
 
 export const loadOffersMainScreen = createAction<OffersList>('cities/loadOffersMainScreen');
 
 export const setOffers = createAction('cities/setOffers');
+
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,17 +1,20 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { setOffers, setCurrentCity, loadOffersMainScreen } from './action';
+import { setOffers, setCurrentCity, loadOffersMainScreen, requireAuthorization } from './action';
 import { Offer, OffersList } from '../types/offer';
 import { fetchOffers, fetchOffersMainScreen } from '../cities-logic';
+import { AuthorizationStatus } from '../const';
 
 
 const initialState: {
   city: string;
   offersMainScreen: OffersList;
   offers: Offer[];
+  authorizationStatus: AuthorizationStatus;
 } = {
   city: 'Paris',
   offersMainScreen: fetchOffersMainScreen(),
   offers: fetchOffers(),
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -24,6 +27,9 @@ const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(setOffers, (state) => {
       state.offers = fetchOffers();
+    })
+    .addCase(requireAuthorization, (state, action) => {
+      state.authorizationStatus = action.payload;
     });
 });
 
