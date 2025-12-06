051d4375d69c8ee53eea5145a5550dc1a58cbf08
CepreNGT <malitevs@gmail.com>
2025-12-06 20:10:50 +0500

    Updated PrivateRoute

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,12 +1,19 @@
 import {Navigate} from 'react-router-dom';
 import {AppRoute, AuthorizationStatus} from '../../const';
+import { useAppSelector } from '../../hooks';
+import LoadingScreen from '../../pages/loading-screen/loading-screen';
 
 type PrivateRouteProps = {
-  authorizationStatus: AuthorizationStatus;
   children: JSX.Element;
 }
 
-function PrivateRoute({authorizationStatus, children}: PrivateRouteProps): JSX.Element {
+function PrivateRoute({children}: PrivateRouteProps): JSX.Element {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  if (authorizationStatus === AuthorizationStatus.Unknown) {
+    return <LoadingScreen />;
+  }
+
   return (
     authorizationStatus === AuthorizationStatus.Auth
       ? children
