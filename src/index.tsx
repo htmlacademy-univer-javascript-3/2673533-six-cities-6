e639fa1882cfea2c603e1ef260d2c308bc5b0243
CepreNGT <malitevs@gmail.com>
2025-12-06 18:07:44 +0500

    Added AuthStatus check when start

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import {checkAuthAction} from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
