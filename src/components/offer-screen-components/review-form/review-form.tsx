34bcf4b1b638d13cdaab8d16d3bf8615f0519599
CepreNGT <malitevs@gmail.com>
2025-12-18 17:38:21 +0500

    Made some little updates

--- src/components/offer-screen-components/review-form/review-form.tsx
+++ src/components/offer-screen-components/review-form/review-form.tsx
@@ -4,6 +4,7 @@ import ReviewStar from '../review-star/review-star';
 import ReviewTextarea from '../review-textarea/review-textarea';
 import { useAppDispatch } from '../../../hooks';
 import { fetchCommentsAction, postCommentAction } from '../../../store/api-actions';
+import { MAX_REVIEW_LENGTH, MIN_REVIEW_LENGTH } from '../../../const';
 
 type ReviewFormProps = {
   offerId: string;
@@ -11,6 +12,7 @@ type ReviewFormProps = {
 
 function ReviewForm({ offerId } : ReviewFormProps): JSX.Element {
   const [rating, setRating] = useState('');
+  const [isBlocked, setIsBlocked] = useState(false);
   const [reviewText, setReviewText] = useState('');
   const dispatch = useAppDispatch();
 
@@ -23,8 +25,9 @@ function ReviewForm({ offerId } : ReviewFormProps): JSX.Element {
   };
 
   const handleOnSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    setIsBlocked(true);
     evt.preventDefault();
-    if (rating && reviewText.length >= 50 && reviewText.length <= 300) {
+    if (rating && reviewText.length >= MIN_REVIEW_LENGTH && reviewText.length <= MAX_REVIEW_LENGTH) {
       dispatch(postCommentAction({
         comment: reviewText,
         rating: Number(rating),
@@ -33,25 +36,26 @@ function ReviewForm({ offerId } : ReviewFormProps): JSX.Element {
         dispatch(fetchCommentsAction(offerId));
       });
     }
+    setIsBlocked(false);
   };
 
   return (
-    <form className="reviews__form form" action="#" method="post" onSubmit={handleOnSubmit}>
+    <form className="reviews__form form" action="#" method="post" onSubmit={handleOnSubmit} >
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <ReviewStar rating="5" title="perfect" selected={rating} onRatingChange={handleRatingChange} />
-        <ReviewStar rating="4" title="good" selected={rating} onRatingChange={handleRatingChange} />
-        <ReviewStar rating="3" title="not bad" selected={rating} onRatingChange={handleRatingChange} />
-        <ReviewStar rating="2" title="badly" selected={rating} onRatingChange={handleRatingChange} />
-        <ReviewStar rating="1" title="terribly" selected={rating} onRatingChange={handleRatingChange} />
+        <ReviewStar rating="5" title="perfect" selected={rating} onRatingChange={handleRatingChange} disabled={isBlocked}/>
+        <ReviewStar rating="4" title="good" selected={rating} onRatingChange={handleRatingChange} disabled={isBlocked}/>
+        <ReviewStar rating="3" title="not bad" selected={rating} onRatingChange={handleRatingChange} disabled={isBlocked}/>
+        <ReviewStar rating="2" title="badly" selected={rating} onRatingChange={handleRatingChange} disabled={isBlocked}/>
+        <ReviewStar rating="1" title="terribly" selected={rating} onRatingChange={handleRatingChange} disabled={isBlocked}/>
       </div>
-      <ReviewTextarea reviewText={reviewText} onReviewTextChange={handleReviewTextChange}/>
+      <ReviewTextarea reviewText={reviewText} onReviewTextChange={handleReviewTextChange} disabled={isBlocked}/>
       <div className="reviews__button-wrapper">
         <ReviewHelp />
         <button
           className="reviews__submit form__submit button"
           type="submit"
-          disabled={!rating || reviewText.length < 50 || reviewText.length > 300}
+          disabled={!rating || reviewText.length < MIN_REVIEW_LENGTH || reviewText.length > MAX_REVIEW_LENGTH || isBlocked}
         >
           Submit
         </button>
--- src/components/offer-screen-components/review-textarea/review-textarea.test.tsx
+++ src/components/offer-screen-components/review-textarea/review-textarea.test.tsx
@@ -3,7 +3,7 @@ import ReviewTextarea from './review-textarea';
 
 describe('Component: ReviewTextarea', () => {
   it('should render correctly', () => {
-    render(<ReviewTextarea reviewText='' onReviewTextChange={vi.fn()}/>);
+    render(<ReviewTextarea reviewText='' onReviewTextChange={vi.fn()} disabled={false}/>);
 
     expect(screen.getByPlaceholderText('Tell how was your stay, what you like and what can be improved')).toBeInTheDocument();
   });
